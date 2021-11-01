7deeedf0731282e9ec5781fb34977e97b25f62b9
k2 <trofimchuk.an@gmail.com>
2021-11-01 09:46:02 +0200

    Add docker support

--- ContactList/obj/Debug/net5.0/ContactList.AssemblyInfo.cs
+++ ContactList/obj/Debug/net5.0/ContactList.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("b24ee6f1-b400-4f07-82b6-7c67af01f837")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
